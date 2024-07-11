8f1e2d45c5e2be08148812edd4fe513227ffa1aa
NanaYaw <ackam.noble@gmail.com>
2024-07-11 14:42:48 +0000

    Add api modules and request specs

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,55 @@
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      skip_before_action :user_authorized, only: [:login]
+      rescue_from ActiveRecord::RecordNotFound, with: :handle_user_record_not_found
+
+      def login
+        user = User.find_by!(email: login_params[:email])
+        if user.authenticate(login_params[:password])
+          @token = encode_token(user_id: user.id)
+          render json: {
+            user:,
+            token: @token
+          }, status: :accepted
+        else
+          render json: { message: 'Incorrect password' }, status: :unauthorized
+        end
+      end
+
+      def destroy
+        session[:user_id] = nil
+        if current_user
+          render json: {
+            status: 200,
+            message: 'logged out successfully'
+          }, status: :ok
+        else
+          render json: {
+            status: 401,
+            message: "Couldn't find an active session."
+          }, status: :unauthorized
+        end
+      end
+
+      private
+
+      def login_params
+        params.permit(:email, :password)
+      end
+
+      def handle_user_record_not_found(_e)
+        render json: { message: 'Please check email | password and try again' },
+               status: :unauthorized
+      end
+
+      def jwt_decode(token)
+        decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS512' })[0]
+        result = ActiveSupport::HashWithIndifferentAccess.new decoded
+        return nil if Time.zone.at(result[:exp]) < Time.zone.now
+
+        result
+      end
+    end
+  end
+end
---
+++ spec/requests/Api/V1/sessions_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+module Api
+  module V1
+    RSpec.describe SessionsController, type: :request do
+      describe '.login' do
+      end
+    end
+  end
+end
