88bbc1ae13ef4d78a562d824de069f44d679333f
NanaYaw <ackam.noble@gmail.com>
2024-07-11 16:34:23 +0000

    Add products model
    Add  app/models/product.rb
    Add  db/schema.rb
    Add  spec/models/product_spec.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_131229) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
+  create_table "orders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "stock"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
