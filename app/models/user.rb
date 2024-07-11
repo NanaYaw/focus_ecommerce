87287ea0b46bf8dd00f1f85ff6c5165a74caf70e
NanaYaw <ackam.noble@gmail.com>
2024-07-11 00:45:25 +0000

    Add user model and  validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
+
+  has_many :sessions
 end
