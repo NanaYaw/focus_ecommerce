e6d9e114fdf36f1b0e2e325bc715fc38d15c0856
NanaYaw <ackam.noble@gmail.com>
2024-07-11 14:24:51 +0000

    Add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  namespace :api do
+    namespace :v1 do
+      post 'login', to: 'sessions#login'
+      post 'logout', to: 'sessions#destroy'
+    end
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
