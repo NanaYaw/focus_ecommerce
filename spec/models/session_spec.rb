8bf34cb230a87073c215e8c1632aec6e1ec56c38
NanaYaw <ackam.noble@gmail.com>
2024-07-12 11:22:37 +0000

    Delete session_spec.rb

--- spec/models/session_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Session, type: :model do
-  subject { build(:session) }
-
-  it { is_expected.to validate_presence_of(:token) }
-  it { is_expected.to validate_presence_of(:user) }
-end
