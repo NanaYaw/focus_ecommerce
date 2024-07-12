d7722eb5dd604bd1f16a9f098c4405b7d8f66fdb
NanaYaw <ackam.noble@gmail.com>
2024-07-12 11:20:57 +0000

    Add Seeds to populate Inititial Data DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,30 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(
+user = User.create!(
   name: "name",
   email: "gus@example.com",
   password: "password",
 )
+
+10.times.each do
+  Product.create(
+    name: Faker::Coffee.name,
+    stock: Faker::Number.between(from: 1, to: 10),
+    price: Faker::Number.between(from: 100, to: 500)
+  )
+end
+
+2.times.each do
+  order = Order.create!(
+    user: User.first,
+  )
+
+  5.times.each do
+    ProductLine.create(
+      quantity: Faker::Number.between(from: 1, to: 3),
+      product_id: Product.all.sample.id,
+      order_id: order.id
+    )
+  end
+end
