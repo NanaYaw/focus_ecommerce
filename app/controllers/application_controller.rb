a34bf13e100ddb04cdae843331507b5188d5a63c
NanaYaw <ackam.noble@gmail.com>
2024-07-12 11:17:14 +0000

    Add login and session managment
    - Add JWT encoding and decoding
    - Add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
+  SECRET_KEY = Rails.application.credentials.to_s
+
   def not_found
     render json: {
       errors: [
@@ -28,5 +30,34 @@ class ApplicationController < ActionController::API
         message.nil? ? default_message : message
       ]
     }, status: :bad_request
+  end
+
+  def encode_token(payload)
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode_user_token
+    header = request.headers['Authorization']
+    return unless header
+
+    @token = header.split[1]
+    begin
+      JWT.decode(@token, SECRET_KEY, true, algorithm: 'HS256')
+    rescue JWT::DecodeError
+      nil
+    end
+  end
+
+  def current_user
+    return if decode_user_token.nil?
+
+    user_id = decode_user_token[0]['user_id']
+    User.find_by(id: user_id)
+  end
+
+  def user_authorized
+    return unless current_user.nil?
+
+    render json: { message: 'Please log in' }, status: :unprocessable_entity
   end
 end
