5329ec8aa8184a63c74ffb0e4ff3781bfd9d0b9c
NanaYaw <ackam.noble@gmail.com>
2024-07-16 06:19:28 +0000

    Add: User Roles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_11_183424) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_055625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_183424) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "user"
     t.index "lower((email)::text)", name: "index_users_on_LOWER_email", unique: true
   end
 
